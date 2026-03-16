d9fd16f0528fd3f44237f7ba757db151910f0e43
Lewis Parrott <lparrott91@gmail.com>
2026-03-16 16:03:36 +0700

    Fix base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://levelledmedia.github.io',
+  base: '/agency',
   output: 'static',
   build: {
     format: 'directory'
